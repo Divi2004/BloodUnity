d5319c8c40cd2da4aa81c9629adb7541176db52a
RiteshBorse <riteshborse008@gmail.com>
2023-08-31 14:36:04 +0530

    UI for signin is completed
    it has a edittext for email and password. there is a back button , a proceed button .

--- app/src/main/java/com/example/blood/SignIn.kt
+++ app/src/main/java/com/example/blood/SignIn.kt
@@ -4,10 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 
-class SignUp : AppCompatActivity() {
+class SignIn : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        setContentView(R.layout.activity_sign_in)
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
     }
 }
