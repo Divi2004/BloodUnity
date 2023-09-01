2afa7a4861e1575837a26e26a07e0063ef90c1cb
Divi2004 <dsbhavsar370122@kkwagh.edu.in>
2023-08-31 22:38:51 -0700

    Code for MainActivity
    Signup, signin button functions

--- app/src/main/java/com/example/blood/MainActivity.kt
+++ app/src/main/java/com/example/blood/MainActivity.kt
@@ -1,16 +1,30 @@
 package com.example.blood
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.blood.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        //id for signin = button_signin
-        //id for signup = button_signup
+        mainBinding= ActivityMainBinding.inflate(layoutInflater)
+        val view= mainBinding.root
+        setContentView(view)
 
+
+       mainBinding.buttonSignup.setOnClickListener {
+           val intent = Intent(this@MainActivity,SignUp::class.java)
+           startActivity(intent)
+
+    }
+        mainBinding.buttonSignin.setOnClickListener {
+            val intent = Intent(this@MainActivity,SignIn::class.java)
+            startActivity(intent)
+        }
 
     }
 }
